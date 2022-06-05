3005a7457baf937acc86e0cb14e79ee8a36bce8d
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 22:19:01 +0300

    creted view function for homepage

--- pinit/views.py
+++ pinit/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profiles,Comment,Images
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    images =Images.get_all_images()
+    return render(request, 'home.html', {'images':images})
