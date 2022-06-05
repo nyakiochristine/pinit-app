a95f16615bb5b9f5862ea96efb1d7ac4e21c086a
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 20:34:33 +0300

    ft:created images class

--- pinit/models.py
+++ pinit/models.py
@@ -11,3 +11,12 @@ class Profile(models.Model):
     bio = models.CharField(max_length=200)
     image =models.ImageField(blank=True)
     user =models.OneToOneField(User,ondelete=models.CASCADE, primary_key=True)
+    
+    
+
+
+class Images(models.Model):
+    image = models.ImageField(blank=True)
+    captions = models.CharField(max_length=120)
+    posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User,ondelete=models.CASCADE)
