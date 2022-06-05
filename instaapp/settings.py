2e32e81e3c3586d07f6658226157a4c78b038d84
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 22:13:41 +0300

    ft:configured postgresql database

--- instaapp/settings.py
+++ instaapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap',
+    'bootstrap5',
     'pinit.apps.PinitConfig',
 
     
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'instaapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'postgresql',
+        'NAME': 'instagram',
+        'USER': 'moringa',
+        'PASSWORD': '1246'
     }
 }
 
