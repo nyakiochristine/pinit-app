48344e6a105cf2bea71c2bc98c0f83f573a46738
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 20:29:27 +0300

    ft:created profile class

--- pinit/models.py
+++ pinit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import  User
+
+
 
 # Create your models here.
+#classes:profile,images, commenting
+
+class Profile(models.Model):
+    bio = models.CharField(max_length=200)
+    image =models.ImageField(blank=True)
+    user =models.OneToOneField(User,ondelete=models.CASCADE, primary_key=True)
