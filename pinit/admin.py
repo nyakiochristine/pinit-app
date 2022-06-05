a687bb9a03f322f45d3a810a8bcebef7240bf4a3
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 20:48:11 +0300

    ft:registered model classes

--- pinit/admin.py
+++ pinit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from  .models import Profile,Comment,Images
+
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Images)
+admin.site.register(Comment)
