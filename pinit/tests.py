f5de5619ba71fb85f5524801c093714a3584cab7
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 21:24:57 +0300

    ft:created a test class for profiles model

--- pinit/tests.py
+++ pinit/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Profiles,Comment,Images
+import datetime as dt
+from django.contrib.auth.models import  User
+
 
 # Create your tests here.
+
+class ProfilesTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username="kris", email="chrismwangi@gmail.com",password="charlie")
+        self.user.save()
+        self.profile = Profile(bio="I am awesome",image="http://",user=self.user)
+        self.profile.save()
+        
+    def test_instance(self):
+        self.asssertTrue(self.profile.Profiles)
+        
+    def test_save_method(self):
+        profile =Profiles.objects.all()
+        self.assertTrue(len(profile) > 0)
+        
+        
+    def test_delete_method(self):
+        self.profile.delete_profile()
+        profile = Profiles.objects.all()
+        self.assertTrue(len(profile) == 0)
+        
+    def tearDown(self):
+        Images.objects.all().delete()
+        
+        
+    
+        
+        
