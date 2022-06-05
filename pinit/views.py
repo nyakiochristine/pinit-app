f55d2e4ffaff533462251f0ab8b12a4b72138e65
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-05 22:24:21 +0300

    creted view function for search bar

--- pinit/views.py
+++ pinit/views.py
@@ -9,3 +9,13 @@ from django.http import HttpResponse
 def home(request):
     images =Images.get_all_images()
     return render(request, 'home.html', {'images':images})
+
+def  search(request):
+    if 'search' in request.GET  and request.GET['search']:
+        search_term = request.GET.get['search']
+        profiles = Profiles.get_profile_by_name(search_term)
+        message = f'{search_term}'
+        return render(request, {'profiles':profiles},{'search_term':search_term})
+    else:
+        message = 'Search Username'
+        return render(request, 'search.html', {'message':message})
