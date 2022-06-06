3bf6127fb7b4476fa0f26f5436148d8e0218d21c
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-06 09:05:17 +0300

    ft:created commenting form nd post images form

--- pinit/forms.py
+++ pinit/forms.py
@@ -3,6 +3,7 @@ from .models import Profiles,Comment,Images
 from django.contrib.auth.forms import AuthentificationForm
 
 
+
 class LoginForm(AuthentificationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'
                                                             'placeholder':'Username'}))
@@ -10,3 +11,23 @@ class LoginForm(AuthentificationForm):
     
 password = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'
                                                         'placeholder':'Password'}))
+
+class PostImageForm(forms.ModelForm):
+    image =forms.ImageField()
+    captions = forms.CharField(max_length=200)
+    
+    class Meta:
+        models = Images
+        exclude = ('posted','profile')
+        
+        
+class  PostCommentForm(forms.ModelForm):
+    class Meta:
+        models = Comment
+        exclude = ('image','posted','user')
+    
+    
+class PostProfileForm(forms.ModelForm):
+    class Meta:
+        models = Profiles
+        exclude = ['user',]
