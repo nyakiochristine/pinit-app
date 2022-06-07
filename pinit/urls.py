648d916c4a39e8168040232ba5761dcce7b54e4d
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-07 10:14:29 +0300

    ft:added url paths

--- pinit/urls.py
+++ pinit/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns =[
-    path('account/', include('django.contrib.auth.urls')),
-    path('',views.home,name='home'),
-    path(r'^search/',views.search,name='search'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('home/',views.home,name='home'),
+    path(r'search/',views.search,name='search'),
+    path(r'user/(?<username>\w+)',views.profile,name='profile'),
+    path(r'postImage/',views.post_image,name='postImage'),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
