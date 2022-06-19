cb75f9041f61aa5b60f67b0b8f24a0e67bbfed72
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-19 20:49:29 +0300

    configuring to heroku

--- instaapp/settings.py
+++ instaapp/settings.py
@@ -37,8 +37,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-Mode = config('MODE',cast=bool)
-MODE=config("MODE", default="dev")
+
+MODE= 'prod'
 
 # Application definition
 
